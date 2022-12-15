4f8eecd97adc9547700f043d07eeb86f3e7f7d6f
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2022-12-14 22:02:31 -0500

    Add source command in comment

--- code_injection.R
+++ code_injection.R
@@ -1,3 +1,6 @@
+# Try calling this script from your RStudio using the command below—
+# source("https://raw.githubusercontent.com/DCEG-workshops/replication-demo/main/code_injection.R")
+
 print("Hello from GitHub!")
 
 greet <- function(name) {
